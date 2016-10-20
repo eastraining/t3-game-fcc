a1f9d110e69f5e6ba953b72a3c06543a8bef9865
Yu Dong Hee <heeyudong@gmail.com>
2016-10-20 18:58:35 +0800

    completed win condition checker

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// global variables
 	var humanPlayer = "";
 	var compPlayer = "";
 	var initTitleHtml = "Choose <a class='color-red'>X</a> or <a class='color-blue'>O</a>";
@@ -6,10 +7,26 @@ $(document).ready(function() {
 	var titleTag = $(".top-title");
 	var tagRed = "<a class='color-red'>";
 	var tagBlue = "<a class='color-blue'>";
+	var humanTag = "";
+	var compTag = "";
 	var closeTag = "</a>";
+	var currentPlayer = "";
+	var running = false;
+	var humanPlaying = true;
+	var gameCell = $(".square-grid__cell");
+	var numCells = 9;
+	var allCells = {};
+	var allRows = [["0", "1", "2"], ["3", "4", "5"], ["6", "7", "8"]];
+	var allColumns = [["0", "3", "6"], ["1", "4", "7"], ["2", "5", "8"]];
+	var allDiagonals = [["0", "4", "8"], ["2", "4", "6"], ["0", "4", "8"]]; // extra diagonal for ease
+
+	// function for reading html
+	function readHTML(tag) {
+		return tag.html();
+	}
 
 	// closure for updating html
-	function updater(tag) {
+	function writeHTML(tag) {
 		function inserter(content) {
 			return tag.html(content);
 		}
@@ -18,20 +35,71 @@ $(document).ready(function() {
 
 	// initial setup
 	function initialise() {
-		updater(titleTag)(initTitleHtml);
+		writeHTML(titleTag)(initTitleHtml);
 		$("a").click(function() {
 			humanPlayer = $(this).html();
 			if (humanPlayer == "X") {
 				compPlayer = "O";
-				updater(titleTag)(gameTitleHtml + tagRed + humanPlayer + closeTag);
+				humanTag = tagRed + humanPlayer + closeTag;
+				compTag = tagBlue + compPlayer + closeTag;
+
 			}
 			else if (humanPlayer == "O") {
 				compPlayer = "X";
-				updater(titleTag)(gameTitleHtml + tagBlue + humanPlayer + closeTag);
+				humanTag = tagBlue + humanPlayer + closeTag;
+				compTag = tagRed + compPlayer + closeTag;
 			}
+			running = true;
+			writeHTML(titleTag)(gameTitleHtml + humanTag);
+			gameCell.click(function() {
+				var that = $(this);
+				if (readHTML(that) == "") {
+					if (humanPlaying == true) {
+						writeHTML(that)(humanTag);
+						humanPlaying = false;
+					}
+				}
+			});
 		});
 	}
 
+	/* 
+	 *	Win Conditions:
+	 *	Returns a string.
+	 *	Returns the winning player if game complete.
+	 *	Else returns "N".
+	 */
+	function isGameComplete() {
+		// determine state of board
+		for (var i = 0; i < numCells; i++) {
+			var temp = "cell" + i.toString();
+			allCells[i] = $("#" + temp).text();
+		}
+
+		// determine if any win condition is met
+		for (var j = 0; j < allRows.length; j++) {
+			if (allCells[allRows[j][0]] != "") {
+				if (allCells[allRows[j][0]] == allCells[allRows[j][1]] && allCells[allRows[j][1]] == allCells[allRows[j][2]]) {
+					return allCells[allRows[j][0]];
+				}
+			}
+			if (allCells[allColumns[j][0]] != "") {
+				if (allCells[allColumns[j][0]] == allCells[allColumns[j][1]] && allCells[allColumns[j][1]] == allCells[allColumns[j][2]]) {
+					return allCells[allColumns[j][0]];
+				}
+			}
+			if (allCells[allDiagonals[j][0]] != "") {
+				if (allCells[allDiagonals[j][0]] == allCells[allDiagonals[j][1]] && allCells[allDiagonals[j][1]] == allCells[allDiagonals[j][2]]) {
+					return allCells[allDiagonals[j][0]];
+				}
+			}
+		}
+
+		// return "N" for no winner
+		return "N";
+	}
+
+	// start game
 	initialise();
-	
+
 });
